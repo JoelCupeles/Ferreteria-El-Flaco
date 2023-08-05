f28acecaf712ae7e9d8a8cf471e970a05ae41d4e
JoelCupeles <joel.cupeles1998@gmail.com>
2023-08-05 16:30:57 -0500

    jbdjd" lkwsxk;lxw
    SZ ,+Ñ.A
    
    GIT ADD a

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ document.addEventListener('DOMContentLoaded', function() {
       window.open('https://goo.gl/maps/WNWc1Bf1dutxyvx67', '_blank');
   });
 });
+
+ // New code
+ const menuCheckbox = document.getElementById('show-menu');
+ const menu = document.querySelector('.content .links');
+
+ // Close the menu when clicking outside of it
+ document.addEventListener('click', function(event) {
+   if (menuCheckbox.checked && !menu.contains(event.target) && event.target !== menuCheckbox) {
+     menuCheckbox.checked = false;
+   }
+ });
+
+ // Close the menu when scrolling
+ window.addEventListener('scroll', function() {
+   if (menuCheckbox.checked) {
+     menuCheckbox.checked = false;
+   }
+ });
